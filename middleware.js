b4a5caedd0e8c754a7ae9672bff031d70fe93f37
sonam tshering <sonamtshering1213@gmail.com>
2024-12-09 10:03:15 +0530

    middleware2

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
 export { default } from "next-auth/middleware";
 export const config = { 
-    matcher: ["/dashboard",'/dashboard/annoucement', '/dashboard/text','/dashboard/adminannoucement'] // Apply middleware to multiple routes
+  matcher: ["/dashboard/:path*"] // Match `/dashboard` and all subroutes dynamically
   };
